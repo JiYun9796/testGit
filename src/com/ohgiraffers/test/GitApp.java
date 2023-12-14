1f94745fe2918b39070fa17a4f470943c3b8c14a
JiYun9796 <pisaek102@gmail.com>
2023-12-14 15:21:53 +0900

    세상 모든 억까 프로젝트

--- src/com/ohgiraffers/test/GitApp.java
+++ src/com/ohgiraffers/test/GitApp.java
@@ -5,5 +5,8 @@ public class GitApp {
         System.out.println("점심은 즉떡으로 먹었는데 저녁은 뭘 먹어야하나..");
         System.out.println("부대찌개를 먹자니 부대 떡볶이를 먹었고..");
         System.out.println("다른걸 먹자니 딱히 생각나는게 없넹");
+        System.out.println("깃헙이 나에게 억까를 줬어..아주 크고 푸짐한 억까를 줬다고요...");
+        System.out.println("이정도 억까면 사탄도 엌ㅋㅋㅋ머임ㅋㅋㅋ하겠네ㅠㅠㅠㅠ");
+        System.out.println("사탄 오늘도 1패 적립!!");
     }
 }
