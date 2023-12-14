dc2d227a757d4c3ebaba8e0d5ea3f0c7ee96ae2f
JiYun9796 <pisaek102@gmail.com>
2023-12-14 15:07:50 +0900

    세상 모든 억까 프로젝트

---
+++ src/com/ohgiraffers/test/GitApp.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.test;
+
+public class GitApp {
+    public static void main(String[] args) {
+        System.out.println("점심은 즉떡으로 먹었는데 저녁은 뭘 먹어야하나..");
+        System.out.println("부대찌개를 먹자니 부대 떡볶이를 먹었고..");
+        System.out.println("다른걸 먹자니 딱히 생각나는게 없넹");
+    }
+}
